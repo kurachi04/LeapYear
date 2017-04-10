faad72e5262a840cac3f9fa029442159ea104f4e
kurachi04 <kurachi04777@gmail.com>
2017-04-10 08:08:35 +0800

    期末作業

---
+++ LeapYear/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LeapYear
+{
+    public partial class Form1 : Form
+    {
+        private LeapYear leapyear = new LeapYear();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            leapyear.Year = int.Parse(textBox1.Text);
+            leapyear.LeapYearCount();
+        }
+    }
+}
---
+++ LeapYear/LeapYear.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeapYear
+{
+    public class LeapYear
+    {
+        public int Year { get; set; }
+
+        /*
+        public LeapYear(int year)
+        {
+            this.Year = year;
+        }
+        */
+
+        public void LeapYearCount()
+        {
+            if (Year % 4 == 0)
+            {
+                Console.WriteLine("{0}是閏年", Year);
+            }
+            else
+            {
+                Console.WriteLine("{0}不是閏年", Year);
+            }
+        }
+    }
+}
